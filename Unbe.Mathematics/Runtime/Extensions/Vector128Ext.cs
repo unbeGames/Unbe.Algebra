af831c7382f78ab5929fd0c90760daa5c7f85fbc
Arugin <valerymayatsky@gmail.com>
2023-09-27 17:27:54 +0300

    Add Shuffle

--- Unbe.Mathematics/Runtime/Extensions/Vector128Ext.cs
+++ Unbe.Mathematics/Runtime/Extensions/Vector128Ext.cs
@@ -1,20 +1,24 @@
-﻿using System.Runtime.Intrinsics;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
 
 namespace Unbe.Math {
   public static class Vector128Ext {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector128<int> Reminder(Vector128<int> left, Vector128<int> right) {
       var n = left / right;
 
       return left - n * right;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector128<uint> Reminder(Vector128<uint> left, Vector128<uint> right) {
       var n = left / right;
 
       return left - n * right;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector128<float> Reminder(Vector128<float> left, Vector128<float> right) {
       var n = left / right;
       n = n.Truncate();
@@ -36,6 +40,42 @@ namespace Unbe.Math {
             (float)System.Math.Truncate(vector[2]),
             (float)System.Math.Truncate(vector[3])
         );
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector128<float> Shuffle(Vector128<float> vector, byte control) {
+      if (Avx.IsSupported) {
+        return Avx.Permute(vector, control);
+      }
+
+      return Shuffle(vector, vector, control);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector128<float> Shuffle(Vector128<float> left, Vector128<float> right, byte control) {
+      if (Sse.IsSupported) {
+        return Sse.Shuffle(left, right, control);
+      }
+
+      return ShuffleSoftware(left, right, control);
+
+      static Vector128<float> ShuffleSoftware(Vector128<float> left, Vector128<float> right, byte control) {
+        const byte e0Mask = 0b_0000_0011, e1Mask = 0b_0000_1100, e2Mask = 0b_0011_0000, e3Mask = 0b_1100_0000;
+
+        int e0Selector = control & e0Mask;
+        float e0 = left.GetElement(e0Selector);
+
+        int e1Selector = (control & e1Mask) >> 2;
+        float e1 = left.GetElement(e1Selector);
+
+        int e2Selector = (control & e2Mask) >> 4;
+        float e2 = right.GetElement(e2Selector);
+
+        int e3Selector = (control & e3Mask) >> 6;
+        float e3 = right.GetElement(e3Selector);
+
+        return Vector128.Create(e0, e1, e2, e3);
       }
     }
 
--- Unbe.Mathematics/Runtime/Extensions/Vector256Ext.cs
+++ Unbe.Mathematics/Runtime/Extensions/Vector256Ext.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.Intrinsics;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
 
 namespace Unbe.Math {
   public static class Vector256Ext {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector256<double> Reminder(Vector256<double> left, Vector256<double> right) {
       var n = left / right;
       n = n.Truncate();
@@ -10,16 +12,54 @@ namespace Unbe.Math {
       return left - n * right;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector256<long> Reminder(Vector256<long> left, Vector256<long> right) {
       var n = left / right;
 
       return left - n * right;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector256<ulong> Reminder(Vector256<ulong> left, Vector256<ulong> right) {
       var n = left / right;
 
       return left - n * right;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector256<double> Shuffle(Vector256<double> vector, byte control) {
+      if (Avx2.IsSupported) {
+        return Avx2.Permute4x64(vector, control);
+      }
+
+      return Shuffle(vector, vector, control);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector256<double> Shuffle(Vector256<double> left, Vector256<double> right, byte control) {
+      if (Avx.IsSupported) {
+        return Avx.Shuffle(left, right, control);
+      }
+
+      return ShuffleSoftware(left, right, control);
+
+      static Vector256<double> ShuffleSoftware(Vector256<double> left, Vector256<double> right, byte control) {
+        const byte e0Mask = 0b_0000_0011, e1Mask = 0b_0000_1100, e2Mask = 0b_0011_0000, e3Mask = 0b_1100_0000;
+
+        int e0Selector = control & e0Mask;
+        double e0 = left.GetElement(e0Selector);
+
+        int e1Selector = (control & e1Mask) >> 2;
+        double e1 = left.GetElement(e1Selector);
+
+        int e2Selector = (control & e2Mask) >> 4;
+        double e2 = right.GetElement(e2Selector);
+
+        int e3Selector = (control & e3Mask) >> 6;
+        double e3 = right.GetElement(e3Selector);
+
+        return Vector256.Create(e0, e1, e2, e3);
+      }
     }
 
     public static Vector256<double> Truncate(this Vector256<double> vector) {
