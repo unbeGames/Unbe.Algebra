6034b08fd2e7536bb3401ffccf7b608fd8ea84fe
Arugin <valerymayatsky@gmail.com>
2023-10-14 14:42:53 +0300

    Added float4x4

--- Unbe.Algebra/Runtime/Matrix/Float4x4.cs
+++ Unbe.Algebra/Runtime/Matrix/Float4x4.cs
@@ -1,13 +1,15 @@
-﻿using System.Runtime.Intrinsics;
-
-namespace Unbe.Algebra {
+﻿namespace Unbe.Algebra {
   namespace Unbe.Algebra {
     [MathType(type: typeof(float), dimensionX: 4, dimensionY: 4)]
     public partial struct Float4x4 /*: IEquatable<Float4x4>, IFormattable*/ {
-      public Vector128<float> c0;
-      public Vector128<float> c1;
-      public Vector128<float> c2;
-      public Vector128<float> c3;
+      /// <summary>Column 0 of the matrix.</summary>
+      public Float4 c0;
+      /// <summary>Column 1 of the matrix.</summary>
+      public Float4 c1;
+      /// <summary>Column 2 of the matrix.</summary>
+      public Float4 c2;
+      /// <summary>Column 3 of the matrix.</summary>
+      public Float4 c3;
     }
   }
 }
