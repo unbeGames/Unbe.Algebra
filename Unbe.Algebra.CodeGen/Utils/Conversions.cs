78226ea7dfedcc16c11a58a6ffd9333477c26692
Arugin <valerymayatsky@gmail.com>
2024-03-13 20:05:29 +0300

    Finished tests for less / greater

--- Unbe.Algebra.CodeGen/Utils/Conversions.cs
+++ Unbe.Algebra.CodeGen/Utils/Conversions.cs
@@ -8,30 +8,33 @@ namespace Unbe.Algebra.CodeGen {
     };
 
     internal static readonly Dictionary<int, string[]> conversionVectorTypes = new() {
-      { 2, new[] {"Float2", "Int2", "UInt2" } },
-      { 3, new[] {"Float3", "Int3", "UInt3" } },
-      { 4, new[] {"Float4", "Int4", "UInt4" } },
+      { 2, new[] { "Bool2", "Float2", "Int2", "UInt2" } },
+      { 3, new[] { "Bool3", "Float3", "Int3", "UInt3" } },
+      { 4, new[] { "Bool4", "Float4", "Int4", "UInt4" } },
     };
 
     // source => destination : type
     private static readonly Dictionary<ValueTuple<string, string>, string> conversions = new() {
       // FLOAT
+      { ("bool", "float"), "explicit" },
       { ("float", "float"), "implicit" },
       { ("int", "float"), "implicit" },
-      { ("bool", "float"), "explicit" },
       { ("uint", "float"), "implicit" },
       { ("double", "float"), "explicit" },
 
+      { ("Bool2", "Float2"), "explicit" },
       { ("Float2", "Float2"), "implicit" },
       { ("Int2", "Float2"), "implicit" },
       { ("UInt2", "Float2"), "implicit" },
       { ("Double2", "Float2"), "explicit" },
 
+      { ("Bool3", "Float3"), "explicit" },
       { ("Float3", "Float3"), "implicit" },
       { ("Int3", "Float3"), "implicit" },
       { ("UInt3", "Float3"), "implicit" },
       { ("Double3", "Float3"), "explicit" },
 
+      { ("Bool4", "Float4"), "explicit" },
       { ("Float4", "Float4"), "implicit" },
       { ("Int4", "Float4"), "implicit" },
       { ("UInt4", "Float4"), "implicit" },
@@ -41,22 +44,25 @@ namespace Unbe.Algebra.CodeGen {
       { ("Vector64<float>", "float"), "implicit" },
 
       // UINT
+      { ("bool", "uint"), "explicit" },
       { ("uint", "uint"), "implicit" },
       { ("int", "uint"), "explicit" },
-      { ("bool", "uint"), "explicit" },
       { ("float", "uint"), "explicit" },
       { ("double", "uint"), "explicit" },
 
+      { ("Bool2", "UInt2"), "explicit" },
       { ("UInt2", "UInt2"), "implicit" },
       { ("Int2", "UInt2"), "explicit" },
       { ("Float2", "UInt2"), "explicit" },
       { ("Double2", "UInt2"), "explicit" },
 
+      { ("Bool3", "UInt3"), "explicit" },
       { ("UInt3", "UInt3"), "implicit" },
       { ("Int3", "UInt3"), "explicit" },
       { ("Float3", "UInt3"), "explicit" },
       { ("Double3", "UInt3"), "explicit" },
 
+      { ("Bool4", "UInt4"), "explicit" },
       { ("UInt4", "UInt4"), "implicit" },
       { ("Int4", "UInt4"), "explicit" },
       { ("Float4", "UInt4"), "explicit" },
@@ -66,22 +72,25 @@ namespace Unbe.Algebra.CodeGen {
       { ("Vector64<uint>", "uint"), "implicit" },
 
       // INT
+      { ("bool", "int"), "explicit" },
       { ("uint", "int"), "explicit" },
       { ("int", "int"), "implicit" },
-      { ("bool", "int"), "explicit" },
       { ("float", "int"), "explicit" },
       { ("double", "int"), "explicit" },
 
+      { ("Bool2", "Int2"), "explicit" },
       { ("UInt2", "Int2"), "explicit" },
       { ("Int2", "Int2"), "implicit" },
       { ("Float2", "Int2"), "explicit" },
       { ("Double2", "Int2"), "explicit" },
 
+      { ("Bool3", "Int3"), "explicit" },
       { ("UInt3", "Int3"), "explicit" },
       { ("Int3", "Int3"), "implicit" },
       { ("Float3", "Int3"), "explicit" },
       { ("Double3", "Int3"), "explicit" },
 
+      { ("Bool4", "Int4"), "explicit" },
       { ("UInt4", "Int4"), "explicit" },
       { ("Int4", "Int4"), "implicit" },
       { ("Float4", "Int4"), "explicit" },
---
+++ Unbe.Algebra.Tests/Vectors/Float.cs
@@ -0,0 +1,30 @@
+﻿namespace Unbe.Algebra.Tests {
+  internal class Float {
+    public static readonly Float4[][] lessGreater = new[] {
+      new [] { new (5), new (14), (Float4)new Bool4(true) },
+      new [] { new (-200), new (-200), (Float4)new Bool4(false) },
+      new [] { new (1.5f), new (0), (Float4)new Bool4(false) },
+      new [] { new (0, 0, 10, 10), new (1, 1, 5f, 5f), (Float4)new Bool4(true, true, false, false) },
+      new [] { new (0, 0, 10, 10), new (-5, -5, 21f, 11f), (Float4)new Bool4(false, false, true, true) },
+    };
+
+    [Theory]
+    [Category("Float")]
+    [TestCaseSource(nameof(lessGreater))]
+    public unsafe void LessTheory(Float4 vector1, Float4 vector2, Float4 expected) {
+      var result = vector1 < vector2;
+      Assert.That((Float4)result, Is.EqualTo(expected));
+    }
+
+    [Theory]
+    [Category("Float")]
+    [TestCaseSource(nameof(lessGreater))]
+    public unsafe void GreaterTheory(Float4 vector1, Float4 vector2, Float4 expected) {
+      var result = vector1 > vector2;
+      if (!vector1.Equals(vector2)) {
+        result = !result;
+      }
+      Assert.That((Float4)result, Is.EqualTo(expected));
+    }
+  }
+}
