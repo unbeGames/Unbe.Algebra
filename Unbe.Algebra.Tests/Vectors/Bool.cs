7d464e95ec95f7c124535e65a67bf9b4c43d5ff3
Arugin <valerymayatsky@gmail.com>
2024-03-13 18:53:09 +0300

    Bool equality and inequality

---
+++ Unbe.Algebra.Tests/Vectors/Bool.cs
@@ -0,0 +1,26 @@
+﻿namespace Unbe.Algebra.Tests {
+  internal class Bool {
+    public static readonly Bool4[][] data = new[] {
+      new Bool4[] { new Bool4(false, true, true, false), new Bool4(false, true, true, false), new Bool4(true) },
+      new Bool4[] { new Bool4(false, true, true, false), new Bool4(true, false, false, true), new Bool4(false) },
+      new Bool4[] { new Bool4(false, true, true, false), new Bool4(true, false, true, false), new Bool4(false, false, true, true) },
+      new Bool4[] { new Bool4(false, true, true, false), new Bool4(false, true, false, true), new Bool4(true, true, false, false) },
+    };
+
+    [Theory]
+    [Category("Bool")]
+    [TestCaseSource(nameof(data))]
+    public unsafe void EqualityTheory(Bool4 vector1, Bool4 vector2, Bool4 expected) {
+      var result = vector1 == vector2;
+      Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Theory]
+    [Category("Bool")]
+    [TestCaseSource(nameof(data))]
+    public unsafe void InEqualityTheory(Bool4 vector1, Bool4 vector2, Bool4 expected) {
+      var result = vector1 != vector2;
+      Assert.That(result, Is.EqualTo(!expected));
+    }
+  }
+}
